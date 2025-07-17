0ebcd8da7c939b868e59cbdf0d026d99377b0c7b
pizzaminded <pizzaminded@users.noreply.github.com>
2025-07-17 14:02:25 +0200

    feat: add application layer exceptions

---
+++ src/Core/User/Application/Exception/UserApplicationException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\User\Application\Exception;
+
+use DomainException;
+
+/**
+ * Parent class for every exception in user application layer.
+ */
+class UserApplicationException extends DomainException
+{
+
+}
